c98985f4e0793804235ea77ebb2e4c7da136699a
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2024-02-13 17:03:46 -0500

    Add version to AppController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_VERSION } from './version';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to Budget API vx.x.x"', () => {
+      expect(appController.getVersion()).toBe(`Welcome to Budget API v${APP_VERSION}`);
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { APP_VERSION } from './version';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Welcome to Budget API v${APP_VERSION}`;
   }
 }
