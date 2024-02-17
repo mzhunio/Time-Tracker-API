9eb4628f5ee5fb688a3e5d7fa5f513e4a4c2d308
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-02-16 21:48:16 -0500

    Add prisma

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { APP_VERSION } from '../src/version';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -16,9 +17,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect(`Welcome to Budget API v${APP_VERSION}`);
   });
 });
